4b72a19691376df5eb992cc51dfa89a5a5f26bd4
Shlok2903 <shlokpatel2903@gmail.com>
2025-06-03 02:03:08 +0530

    test

---
+++ hooks/use-window-width.tsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react'
+
+export function useWindowWidth() {
+  const [width, setWidth] = useState<number | undefined>(undefined)
+
+  useEffect(() => {
+    // Set initial width
+    setWidth(window.innerWidth)
+
+    // Add event listener
+    const handleResize = () => setWidth(window.innerWidth)
+    window.addEventListener('resize', handleResize)
+
+    // Cleanup
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  return width
+} 
